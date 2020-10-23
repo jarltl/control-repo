efe749976a1ea2bcf5bb97a8b4b8c3a7c6d458de
jarltl <72981296+jarltl@users.noreply.github.com>
2020-10-23 17:49:35 +0200

    Update server.pp

--- site-modules/profile/manifests/consul/server.pp
+++ site-modules/profile/manifests/consul/server.pp
@@ -8,14 +8,14 @@ class profile::consul::server {
     version     => '1.6.2',
     config_hash => {
       'bootstrap_expect' => 3,
-	  'client_addr'      => '0.0.0.0',
+	   'client_addr'      => '0.0.0.0',
       'data_dir'         => '/opt/consul',
       'datacenter'       => 'NTNU',
       'log_level'        => 'INFO',
       'node_name'        => $facts['hostname'],
       'server'           => true,
       'retry_join'       => [ $::serverip ],
-	  'ui'				 => true,
+	   'ui'				 => true,
     },
     require     => Package['unzip'],
   }
