117a869e07f21a487acccb3baf8d86004d4028b6
jarltl <72981296+jarltl@users.noreply.github.com>
2020-10-23 17:44:42 +0200

    Update server.pp
    Endret server.pp consul for web UI

--- site-modules/profile/manifests/consul/server.pp
+++ site-modules/profile/manifests/consul/server.pp
@@ -8,12 +8,14 @@ class profile::consul::server {
     version     => '1.6.2',
     config_hash => {
       'bootstrap_expect' => 3,
+	  'client_addr'      => '0.0.0.0',
       'data_dir'         => '/opt/consul',
       'datacenter'       => 'NTNU',
       'log_level'        => 'INFO',
       'node_name'        => $facts['hostname'],
       'server'           => true,
       'retry_join'       => [ $::serverip ],
+	  'ui'				 => true,
     },
     require     => Package['unzip'],
   }
